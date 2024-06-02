58f1b08f4dafe413c6d4e46c29b3c8b0158d4a2a
Jatin Verma <jatin28jat@gmail.com>
2024-06-02 12:43:27 +0530

    Initial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBF2rNvaItuNADWWhN_oL_uXYajdC63xi0",
+    authDomain: "whatsappmern-cbd90.firebaseapp.com",
+    projectId: "whatsappmern-cbd90",
+    storageBucket: "whatsappmern-cbd90.appspot.com",
+    messagingSenderId: "602963126888",
+    appId: "1:602963126888:web:1e23a585b2d5a5c78ea9dd"
+  };
